81a8d23b406dbfbc160adb4026b07fba2102f06e
jonson <zhuimengshaonian04@gmail.com>
2020-03-05 18:34:23 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dreamerjackson/minidiv
+
+go 1.13
+
+require github.com/dreamerjackson/mydiv v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dreamerjackson/mydiv"
+)
+
+func main(){
+	res,_ :=mydiv.Div(4,2)
+	fmt.Println(res)
+}
